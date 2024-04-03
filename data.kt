0afa481887af005dc0d864c3f65097ec0b988293
Zahar <zahar210200@gmail.com>
2024-04-03 14:40:18 +0300

    qwe2

--- data.kt
+++ data.kt
@@ -1,48 +1,48 @@
 private const val CONFERENCE_DAY1_START = "2019-05-07T07:00:00-07:00"
-	private const val CONFERENCE_DAY1_END = "2019-05-07T22:00:01-07:00"
-	val TestConferenceDays = listOf(
-    	ConferenceDay(
-        	ZonedDateTime.parse(CONFERENCE_DAY1_START)),
-    	ConferenceDay(
-        	ZonedDateTime.parse(CONFERENCE_DAY2_START),
-        	ZonedDateTime.parse(CONFERENCE_DAY2_END)),
-    	ConferenceDay(
-        	ZonedDateTime.parse(CONFERENCE_DAY3_START),
-        	ZonedDateTime.parse(CONFERENCE_DAY3_END)))
-	val androidTag = Tag("1", Tag.CATEGORY_TOPIC, "track_android", 0, "Android", 0xFFAED581.toInt())
-	val tagsList = listOf(
-    	androidTag, cloudTag, webTag, sessionsTag, codelabsTag, beginnerTag,
-    	intermediateTag, advancedTag, themeTag)
-	val speaker1 = Speaker(
-    	id = "1")
-	val speaker3 = Speaker(
-    	id = "3",
-    	name = "Hans Moleman",
-    	imageUrl = "",
-    	company = "",
-    	biography = "")
-	val room = Room(id = "1", name = "Tent 1")
-	val session0 = Session(
-    	id = "0", title = "Session 0", description = "This session is awesome",
-    	startTime = TestConferenceDays[0].start, endTime = TestConferenceDays[0].end,
-    	isLivestream = false,
-    	room = room, sessionUrl = "", youTubeUrl = "", photoUrl = "", doryLink = "",
-    	tags = listOf(androidTag, webTag, sessionsTag),
-    	displayTags = listOf(androidTag, webTag),
-    	speakers = setOf(speaker1), relatedSessions = emptySet())
-	val sessionsList = listOf(session0, session1, session2, session3, sessionWithYoutubeUrl)
-	val sessionIDs = sessionsList.map { it.id }.toList()
-	val block1 = Block(
-    	title = "Keynote",
-    	type = "keynote",
-    	color = 0xffff00ff.toInt(),
-    	startTime = TestConferenceDays[0].start,
-    	endTime = TestConferenceDays[0].start.plusHours(1L))
-	val block2 = Block(
-    	title = "Breakfast",
-    	type = "meal",
-    	color = 0xffff00ff.toInt(),
-    	startTime = TestConferenceDays[0].start.plusHours(1L),
-    	endTime = TestConferenceDays[0].start.plusHours(2L))
-	val agenda = listOf(block1, block2)
-	private val userEvent0 = UserEvent()
+private const val CONFERENCE_DAY1_END = "2019-05-07T22:00:01-07:00"
+val TestConferenceDays = listOf(
+	ConferenceDay(
+		ZonedDateTime.parse(CONFERENCE_DAY1_START)),
+	ConferenceDay(
+		ZonedDateTime.parse(CONFERENCE_DAY2_START),
+		ZonedDateTime.parse(CONFERENCE_DAY2_END)),
+	ConferenceDay(
+		ZonedDateTime.parse(CONFERENCE_DAY3_START),
+		ZonedDateTime.parse(CONFERENCE_DAY3_END)))
+val androidTag = Tag("1", Tag.CATEGORY_TOPIC, "track_android", 0, "Android", 0xFFAED581.toInt())
+val tagsList = listOf(
+	androidTag, cloudTag, webTag, sessionsTag, codelabsTag, beginnerTag,
+	intermediateTag, advancedTag, themeTag)
+val speaker1 = Speaker(
+	id = "1")
+val speaker3 = Speaker(
+	id = "3",
+	name = "Hans Moleman",
+	imageUrl = "",
+	company = "",
+	biography = "")
+val room = Room(id = "1", name = "Tent 1")
+val session0 = Session(
+	id = "0", title = "Session 0", description = "This session is awesome",
+	startTime = TestConferenceDays[0].start, endTime = TestConferenceDays[0].end,
+	isLivestream = false,
+	room = room, sessionUrl = "", youTubeUrl = "", photoUrl = "", doryLink = "",
+	tags = listOf(androidTag, webTag, sessionsTag),
+	displayTags = listOf(androidTag, webTag),
+	speakers = setOf(speaker1), relatedSessions = emptySet())
+val sessionsList = listOf(session0, session1, session2, session3, sessionWithYoutubeUrl)
+val sessionIDs = sessionsList.map { it.id }.toList()
+val block1 = Block(
+	title = "Keynote",
+	type = "keynote",
+	color = 0xffff00ff.toInt(),
+	startTime = TestConferenceDays[0].start,
+	endTime = TestConferenceDays[0].start.plusHours(1L))
+val block2 = Block(
+	title = "Breakfast",
+	type = "meal",
+	color = 0xffff00ff.toInt(),
+	startTime = TestConferenceDays[0].start.plusHours(1L),
+	endTime = TestConferenceDays[0].start.plusHours(2L))
+val agenda = listOf(block1, block2)
+private val userEvent0 = UserEvent()
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import re
 import math
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
 
 
 def read_text_file(file_path="data.kt"):
@@ -12,6 +13,13 @@ def read_text_file(file_path="data.kt"):
         print("Файл не найден.")
         return None
     return txt
+
+
+def open_file():
+    file_path = filedialog.askopenfilename(filetypes=[("Kotlin Files", ".kt"), ("Text Files", "*.txt"), ("All Files", "*.*")])
+    if file_path:
+        text.delete("1.0", END)
+        text.insert(END, read_text_file(file_path))
 
 
 def count_occurrences(reg1, reg2, txt):
@@ -28,28 +36,39 @@ def count_occurrences(reg1, reg2, txt):
     return occurrence_count
 
 
+def calculate_metrics():
+    global result1, result2
+    text_content = text.get("1.0", END)
+    result1 = count_occurrences(regex1, regex2, text_content)
+    result2 = count_occurrences(regex3, regex4, text_content)
+    result1["Итого"] = sum(result1.values())
+    result2["Итого"] = sum(result2.values())
+    n = len(result1) + len(result2)
+    output_text = f"Словарь программы. n = {n}\n"
+    N = sum(result1.values()) + sum(result2.values())
+    output_text += f"Длина программы. N = {N}\n"
+    V = N * math.log(n, 2)
+    output_text += f"Объем программы. V = {int(V)}\n"
+    output.delete("1.0", END)
+    output.insert(END, output_text)
+    update_treeview()
+
+
+def update_treeview():
+    tree1.delete(*tree1.get_children())
+    for item in result1.items():
+        tree1.insert("", END, values=item)
+
+    tree2.delete(*tree2.get_children())
+    for item in result2.items():
+        tree2.insert("", END, values=item)
+
+
 regex1 = r"(?<=val|var)+\s+[a-zA-Z]+\s*(?=[\:=]*)"   # переменные
 regex2 = r"(?<=\()+\s*[a-zA-Z1-9\"._]+\s*(?=\)*)"  # параметры функций
 
 regex3 = r"\s*(\+|-|\*|\/|%|==|!=|>|<|>=|<=|=|\+=|-=|\*=|\/=|%=|&&|\|\||!|\+\+|--|\.\.|\.|\(\)|\)|\(|if|for|when|while|do|\[.+\])(?=\s*)"  # операторы
 regex4 = r"\s*[a-zA-ZА-я\w\d<>?\t\n\r]+\([\.\s\d\w\s,\"\'=.:!?\[\]\n\r\t]*\)(?=\s*)"  # функции
-
-text = read_text_file()
-
-result1 = count_occurrences(regex1, regex2, text)
-result2 = count_occurrences(regex3, regex4, text)
-
-result1["Итого"] = sum(result1.values())
-result2["Итого"] = sum(result2.values())
-
-n = len(result1) + len(result2)
-print(f"Словарь программы. n = {n}")
-
-N = sum(result1.values()) + sum(result2.values())
-print(f"Длина программы. N = {N}")
-
-V = N*math.log(n, 2)
-print(f"Объем программы. V = {int(V)}")
 
 root1 = Tk()
 root1.title("")
@@ -57,37 +76,62 @@ root1.geometry("1050x750")
 root1.rowconfigure(index=0, weight=1)
 root1.columnconfigure(index=0, weight=1)
 
-tree1 = ttk.Treeview(columns=["operands", "number"], show="headings")
+frame = Frame(root1)
+frame.pack(pady=10)
+
+select_button = Button(frame, text="Выбрать файл", command=open_file)
+select_button.pack(side=LEFT)
+
+calculate_button = Button(frame, text="Рассчитать", command=calculate_metrics)
+calculate_button.pack(side=LEFT)
+
+text_frame = Frame(root1)
+text_frame.pack(fill=BOTH, expand=True)
+
+scrollbar = Scrollbar(text_frame)
+scrollbar.pack(side=RIGHT, fill=Y)
+
+text = Text(text_frame, height=15, yscrollcommand=scrollbar.set)
+text.pack(fill=BOTH, expand=True)
+
+scrollbar.config(command=text.yview)
+
+output_frame = Frame(root1)
+output_frame.pack(fill=BOTH, expand=True)
+
+output_label = Label(output_frame, text="Результат:")
+output_label.pack()
+
+output_scrollbar = Scrollbar(output_frame)
+output_scrollbar.pack(side=RIGHT, fill=Y)
+
+output = Text(output_frame, height=3, yscrollcommand=output_scrollbar.set)
+output.pack(fill=BOTH, expand=True)
+
+scrollbar.config(command=output.yview)
+
+tree_frame = Frame(root1)
+tree_frame.pack(fill=BOTH, expand=True)
+
+tree1 = ttk.Treeview(tree_frame, columns=["operands", "number"], show="headings", height=15)
 tree1.grid(row=0, column=0, sticky="nsew")
-
-
 tree1.heading("operands", text="Операнд")
 tree1.heading("number", text="Количество вхождений")
-
 tree1.column("#1", stretch=NO, width=300)
 tree1.column("#2", stretch=NO, width=200)
 
-for item in result1.items():
-    tree1.insert("", END, values=item)
-
-scrollbar1 = ttk.Scrollbar(orient=VERTICAL, command=tree1.yview)
+scrollbar1 = ttk.Scrollbar(tree_frame, orient=VERTICAL, command=tree1.yview)
 tree1.configure(yscrollcommand=scrollbar1.set)
 scrollbar1.grid(row=0, column=1, sticky="ns")
 
-
-tree2 = ttk.Treeview(columns=["operators", "number"], show="headings")
+tree2 = ttk.Treeview(tree_frame, columns=["operators", "number"], show="headings")
 tree2.grid(row=0, column=2, sticky="nsew")
-
 tree2.heading("operators", text="Оператор")
 tree2.heading("number", text="Количество вхождений")
-
 tree2.column("#1", stretch=NO, width=300)
 tree2.column("#2", stretch=NO, width=200)
 
-for item in result2.items():
-    tree2.insert("", END, values=item)
-
-scrollbar2 = ttk.Scrollbar(orient=VERTICAL, command=tree2.yview)
+scrollbar2 = ttk.Scrollbar(tree_frame, orient=VERTICAL, command=tree2.yview)
 tree2.configure(yscrollcommand=scrollbar2.set)
 scrollbar2.grid(row=0, column=3, sticky="ns")
 
